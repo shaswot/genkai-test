026187372edadbf16672d36287710968f1c83e1d
shaswot <shaswot@genkai>
2024-08-23 14:12:32 +0900

    added genkai test scripts

---
+++ genkai-minicamp/script/container-test/container-test.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+#PJM -L rscgrp=b-batch
+#PJM -L gpu=1
+#PJM -L elapse=0:10:00
+#PJM -L jobenv=singularity
+#PJM -j
+
+module load singularity-ce
+singularity exec --nv ~/genkai-test-bash.sif python3 ./container-test.py 
