e9e65941a2b3f9eddb0e416a4254eca0132427db
shaswot <shaswot@genkai>
2024-08-23 15:37:59 +0900

    sb3arg run in genkai

--- genkai-minicamp/script/sb3args-test/sb3args-test.sh
+++ genkai-minicamp/script/sb3args-test/sb3args-test.sh
@@ -6,4 +6,13 @@
 #PJM -j
 
 module load singularity-ce
-singularity exec --nv ~/genkai-test-bash.sif python3 ./sb3args-test.py 
+singularity exec --nv ~/genkai-test-bash.sif python3 ./sb3args-test.py --exp_no=5 --ALGO=PPO 
+
+
+#python3 ./sb3args-test.py --exp_no=0 \
+#                            --ALGO=A2C \
+#                            --env_id=BreakoutNoFrameskip-v4 \
+#                            --SEED=0 \
+#                            --NUM_ENVS=16\
+#                            --TRAIN_STEPS=10_000\
+#                            --N_EVAL_EPISODES=100
